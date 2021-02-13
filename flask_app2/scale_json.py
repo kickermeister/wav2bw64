05fea5810da3d5244a5edee21962f804a4809f1e
Michael Weitnauer <weitnauer@irt.de>
2021-02-13 22:37:46 +0100

    added comments and improved script

--- flask_app2/scale_json.py
+++ flask_app2/scale_json.py
@@ -8,6 +8,7 @@ def scale_json(filename):
 
     json_content = json.loads(file_content)
     data = json_content["data"]
+    channels = json_content["channels"]
     # number of decimals to use when rounding the peak value
     digits = 2
 
@@ -15,9 +16,12 @@ def scale_json(filename):
     new_data = []
     for x in data:
         new_data.append(round(x / max_val, digits))
-
-    deinterleaved_data = deinterleave(new_data, json_content["channels"])
-    json_content["data"] = deinterleaved_data
+    # audiowaveform is generating interleaved peak data when using the --split-channels flag, so we have to deinterleave it
+    if channels > 1:
+        deinterleaved_data = deinterleave(new_data, channels)
+        json_content["data"] = deinterleaved_data
+    else:
+        json_content["data"] = new_data
     file_content = json.dumps(json_content, separators=(',', ':'))
 
     with open(filename, "w") as f:
@@ -25,8 +29,11 @@ def scale_json(filename):
 
 
 def deinterleave(data, channelCount):
+    # first step is to separate the values for each audio channel and min/max value pair, hence we get an array with channelCount * 2 arrays
     deinterleaved = [data[idx::channelCount * 2] for idx in range(channelCount * 2)]
     new_data = []
+
+    # this second step combines each min and max value again in one array so we have one array for each channel
     for ch in range(channelCount):
         idx1 = 2 * ch
         idx2 = 2 * ch + 1
