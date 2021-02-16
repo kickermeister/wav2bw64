248997297f1aebc5bf4b3f28b46cb196f1037ddf
Michael Weitnauer <weitnauer@irt.de>
2021-02-16 23:36:42 +0100

    render wavewform only on request from frontend via AJAX

--- flask_app2/app.py
+++ flask_app2/app.py
@@ -44,20 +44,25 @@ def upload_file():
 def show_wav_info(filename):
     path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     wav_info = get_wav_info(path)
+    return render_template('wav-info.html',
+                           path=path,
+                           filename=filename,
+                           wav_info=wav_info)
+
+@app.route('/render_waveform', methods=['POST', 'GET'])
+def render_waveform():
+    print(request.form)
+    filename = request.form['filename']
+    path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    wav_info = get_wav_info(path)
     if wav_info["Channels"] > 8:
-        flash('Supporting only WAV files with up to 8 channels')
-        return redirect("/")
+        return 'Waveform rendering supported only for WAV files with up to 8 channels', 400
     else:
         jsonpath = os.path.splitext(path)[0] + '.json'
         cmd = ["audiowaveform", "-i", path, "-o", jsonpath, "--pixels-per-second", "30", "--bits", "8", "--split-channels"]
         _ = subprocess.run(cmd)
         scale_json(jsonpath)
-        return render_template('wav-info.html',
-                               path=path,
-                               jsonpath=jsonpath,
-                               filename=filename,
-                               wav_info=wav_info)
-
+        return jsonify({"jsonpath": jsonpath})
 
 @app.route('/uploads/<filename>')
 def uploads(filename):
