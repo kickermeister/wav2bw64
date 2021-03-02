86ff2b8ad3d6a715fc96a897bc9b18c758f063cf
Michael Weitnauer <weitnauer@irt.de>
2021-03-02 14:47:32 +0100

    fix import colon

--- wav2bw64/svelte_app/src/components/AudioProgramme.svelte
+++ wav2bw64/svelte_app/src/components/AudioProgramme.svelte
@@ -1,7 +1,7 @@
 <script>
   import { ADMStore } from '../stores.js';
   import AudioObject from './AudioObject.svelte'
-  import { Select, Container, Row, Col, TextField, ListItemGroup, ListItem, ExpansionPanels, ExpansionPanel, } from 'svelte-materialify/src';
+  import { Select, Container, Row, Col, TextField, ListItemGroup, ListItem, ExpansionPanels, ExpansionPanel } from 'svelte-materialify/src';
   import { getValidLayouts } from '../adm_utils.js';
   import { mapISO6391 } from '../utils.js';
 
