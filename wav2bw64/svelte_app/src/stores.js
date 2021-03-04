0e0f3790f32c8c19de53b0b884489b8a5606294b
Michael Weitnauer <weitnauer@irt.de>
2021-03-04 15:52:37 +0100

    use more reliable IDs

--- wav2bw64/svelte_app/src/stores.js
+++ wav2bw64/svelte_app/src/stores.js
@@ -4,7 +4,7 @@ export const wav_channels = writable();
 
 function getAudioProgrammeStructure(){
   return {
-    id: Math.random(),
+    id: ID(),
     name: "Give me a name", 
     loudness: -23,
     items: [],
@@ -12,32 +12,38 @@ function getAudioProgrammeStructure(){
   }
 }
 
+export function ID(){
+  // Math.random should be unique because of its seeding algorithm.
+  // Convert it to base 36 (numbers + letters), and grab the first 9 characters after the decimal.
+  return Math.random().toString(36).substr(2, 9);
+}
+
 
 function Store() {
   const { subscribe, set, update } = writable(
     [
       {
-        id: 0,
+        id: ID(),
         name: "Audio Programme 1",
         loudness: -23,
         items: [
-          {id: 0, name: "My Object 1" , type: "Object", routing: "1 - 1"},
-          {id: 1, name: "Stereo Bed", type: "0+2+0", routing: "1 - 2"},
+          {id: ID(), name: "My Object 1" , type: "Object", routing: "1 - 1"},
+          {id: ID(), name: "Stereo Bed", type: "0+2+0", routing: "1 - 2"},
         ],
         language: 'de'
       },
       {
-        id: 1,
+        id: ID(),
         name: "Audio Programme 2",
         loudness: -23,
         items: [
-          {id: 0, name: "Surround Bed", type: "0+5+0", routing: "1 - 6"},
-          {id: 1, name: "Music", type: "0+2+0", routing: "1 - 2"},
-          {id: 2, name: "Dialog", type: "Object", routing: "7 - 7"},
-          {id: 3, name: "Commentary", type: "Object", routing: "8 - 8"}, 
-          {id: 4, name: "7.1 Bed", type: "0+7+0", routing: "1 - 8"},
-          {id: 5, name: "Effect", type: "Object", routing: "8 - 8"},
-          {id: 6, name: "Music Bed 2", type: "0+2+0", routing: "1 - 2"} 
+          {id: ID(), name: "Surround Bed", type: "0+5+0", routing: "1 - 6"},
+          {id: ID(), name: "Music", type: "0+2+0", routing: "1 - 2"},
+          {id: ID(), name: "Dialog", type: "Object", routing: "7 - 7"},
+          {id: ID(), name: "Commentary", type: "Object", routing: "8 - 8"}, 
+          {id: ID(), name: "7.1 Bed", type: "0+7+0", routing: "1 - 8"},
+          {id: ID(), name: "Effect", type: "Object", routing: "8 - 8"},
+          {id: ID(), name: "Music Bed 2", type: "0+2+0", routing: "1 - 2"} 
         ],
         language: 'en'
       },
