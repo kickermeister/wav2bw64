25e804ce671bd0a5db254433c70ac82ab08b5923
Michael Weitnauer <michael.weitnauer@gmail.com>
2021-03-14 20:59:48 +0100

    fix: use iso639-2 language codes to be recognized by the EPS

--- wav2bw64/svelte_app/src/utils.js
+++ wav2bw64/svelte_app/src/utils.js
@@ -1,18 +1,16 @@
-import ISO6391 from 'iso-639-1';
+import langs from 'langs';
 
 
 export function mapISO6391(){
   let languages = [];
-  let names = ISO6391.getAllNames();
-  for (const name of names){
-    languages.push({value: ISO6391.getCode(name), name: name});
+  for (const lang of langs.all()){
+    languages.push({value: lang["2B"], name: lang["name"]});
   }
   languages.sort(function(a, b){
     if(a.name < b.name) { return -1; }
     if(a.name > b.name) { return 1; }
     return 0;
   });
-  // console.log("ISO639 mapped!", languages);
   return languages;
 }
 
